1f41524b1f6bda667abdc3c8cb937d8478d6a005
boablock <boablockchain@gmail.com>
2022-11-28 12:04:41 -0300

    .

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar fondo_navbar navbar-expand-lg bg-light">
+  <nav className="navbar fondo_navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
