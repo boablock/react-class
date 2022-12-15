a6c4fca9a9873d711665ab12282f8ac249dc1de2
boablock <boablockchain@gmail.com>
2022-12-15 12:41:57 -0300

    components-2-ready

--- src/App.js
+++ src/App.js
@@ -12,28 +12,28 @@ import { useState } from 'react';
 function App() {
 
   
-    // const cards = [
-    //     {
-    //         title:'Hello, im a component',
-    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    //         img:'https://ar.all.biz/img/ar/catalog/21448.jpeg',
-    //         btnText:'Comprar',
-    //         btnClassName: 'btn btn-danger fs-1' 
-    //     },
-    //     {
-    //         title:'Hello, im another',
-    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    //         img:'https://imagenes.elpais.com/resizer/JY_bIHi9LoJ-PF8YVIoVNF1OVGQ=/1960x1103/ep01.epimg.net/elpais/imagenes/2014/08/22/videos/1408718239_108049_1408721679_noticia_fotograma.jpg',
-    //         btnText:'Comprar',
-    //     },
-    //     {
-    //         title:'Hello, im another',
-    //         description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    //         img:'https://img-aws.ehowcdn.com/750x400p/cdn-write.demandstudios.com/upload//0000/300/00/6/60306.jpg',
-    //         btnText:'Ver mas',
-    //     },
+    const cards = [
+        {
+            title:'Hello, im a component',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            img:'https://ar.all.biz/img/ar/catalog/21448.jpeg',
+            btnText:'Comprar',
+            btnClassName: 'btn btn-danger fs-1' 
+        },
+        {
+            title:'Hello, im another',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            img:'https://imagenes.elpais.com/resizer/JY_bIHi9LoJ-PF8YVIoVNF1OVGQ=/1960x1103/ep01.epimg.net/elpais/imagenes/2014/08/22/videos/1408718239_108049_1408721679_noticia_fotograma.jpg',
+            btnText:'Comprar',
+        },
+        {
+            title:'Hello, im another',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            img:'https://img-aws.ehowcdn.com/750x400p/cdn-write.demandstudios.com/upload//0000/300/00/6/60306.jpg',
+            btnText:'Ver mas',
+        },
         
-    // ]
+    ]
 
   return (
     <div className="App">
@@ -54,11 +54,11 @@ function App() {
         </a>
       </header> */}
       <Counter nombre='Componente 1'/>
-      <Counter nombre='Componente 2'/>
-      <Counter nombre='Componenete 3'/>
+      {/* <Counter nombre='Componente 2'/>
+      <Counter nombre='Componenete 3'/> */}
       <div className='d-flex justify-content-around'>
     
-        {/* { cards.map(({title,description,img,btnText,btnClassName},index) => (
+        { cards.map(({title,description,img,btnText,btnClassName},index) => (
             <Card
             key ={index} // atributo unico para identificar el objeto en el arreglo (posicion en el caso de index); tmb se puede generar un atributo id en cada uno de ellos. 
             title={title}
@@ -68,7 +68,7 @@ function App() {
             btnClassName ={btnClassName}
             Fiufiu = {Navbar}
             />)
-        )} */}
+        )}
 
       {/* <Card
         title={'Hello, im a component'}
