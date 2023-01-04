f281f2056587f94c63688de49660e0c351292860
boablock <boablockchain@gmail.com>
2023-01-04 19:50:06 -0300

    .

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useParams} from 'react-router-dom'
+
+const ItemDetailContainer = () => {
+  return (
+    <h1>ItemDetailContainerrrrrrrrrrrrrrrrrrr</h1>
+  )
+}
+
+export default ItemDetailContainer
